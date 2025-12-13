86ec98f9abee77d138631c057247db4c5068d7c6
flashgnash <flashgnash@proton.me>
2025-12-13 17:57:33 +0000

    GaugeType in database

---
+++ Migrations/20251207150249_GaugeType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordis.Migrations
+{
+    /// <inheritdoc />
+    public partial class GaugeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GaugeType",
+                table: "Gauges",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GaugeType",
+                table: "Gauges");
+        }
+    }
+}
--- Models/gauge.cs
+++ Models/gauge.cs
@@ -2,12 +2,30 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
+public enum GaugeType {
+	Orb,
+	IconBar
+}
+
+
 public class Gauge {
+
+	[NotMapped]
+	public static readonly Dictionary<string, GaugeType> GaugeTypeLookup =
+	    new()
+	    {
+	        { "health", GaugeType.Orb },
+	        { "mana", GaugeType.Orb },
+	        { "hunger", GaugeType.IconBar }
+	    };
+
 
 	// public string Id => $"{Owner}_{Name}";
 
 	[Key]
 	public Guid Id {get; set;}
+
+	public GaugeType GaugeType {get; set;}
 
 	public int PlayerCharacterId {get; set;}
 
