959ea56c838f491eb4abe36fc67dfb5702439a92
flashgnash <flashgnash@proton.me>
2025-12-26 15:36:30 +0000

    Delete button to remove players from campaigns

--- Services/CampaignService.cs
+++ Services/CampaignService.cs
@@ -3,13 +3,36 @@ using Microsoft.EntityFrameworkCore;
 public class CampaignService(IDbContextFactory<OrdisContext> dbFactory)
 {
 
+    public async Task RemovePlayerAsync(Campaign campaign, PlayerCharacter player) {
+        
+        var db = await dbFactory.CreateDbContextAsync();
+
+        var fetchedCampaign = await db.Campaigns.Include(c => c.Players).FirstOrDefaultAsync(c => c.Id == campaign.Id);
+
+
+        var pc = fetchedCampaign.Players.First(p => p.Id == player.Id);
+        fetchedCampaign.Players.Remove(pc);
+
+        Console.WriteLine($"Removing player {player.Name} from {fetchedCampaign.Name}");
+
+        db.Campaigns.Update(fetchedCampaign);
+
+        await db.SaveChangesAsync();
+        Console.WriteLine("Done");
+        
+    }
+
     public async Task UpdateAsync(Campaign c)
     {
         var db = await dbFactory.CreateDbContextAsync();
 
         db.Campaigns.Update(c);
 
+        Console.WriteLine($"Updating campaign {c.Name}");
+
         await db.SaveChangesAsync();
+
+        Console.WriteLine("Done");
 
     }
     public async Task DeleteAsync(int campaignId) {
